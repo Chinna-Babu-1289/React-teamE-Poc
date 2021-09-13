e2d4f5ae46e977f452ea094a11550d746bb929de
Chinna-Babu-1289 <vanarasichinnababu@gmail.com>
2021-09-13 12:02:50 +0530

    added notfound page

---
+++ task1/src/Components/NavBar/Routers.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
+import Home from "../Pages/Home/Home";
+import Profile from "../Pages/Profile/Profile";
+import LoginForm from "../Pages/Login/LoginForm";
+import Logout from "../Pages/Logout/Logout";
+import SignUpForm from "../Pages/Login/SignUpForm";
+import AddTask from "../Pages/Home/AddTask";
+import Display from "../Pages/Home/Display";
+import NavBar from "./NavBar";
+import NotFound from "../Pages/Home/NotFound";
+
+const Routers = () => {
+  return (
+    <div>
+      <Router>
+        <NavBar />
+        <Switch>
+          <Route path="/" exact component={Home} />
+          <Route path="/profile" component={Profile} />
+          <Route path="/login" component={LoginForm} />
+          <Route path="/logout" component={Logout} />
+          <Route path="/signup" component={SignUpForm} />
+          <Route path="/addtask" component={AddTask} />
+          <Route path="/display" component={Display} />
+          <Route path="*" component={NotFound} />
+        </Switch>
+      </Router>
+    </div>
+  );
+};
+
+export default Routers;
---
+++ task1/src/Components/Pages/Home/NotFound.jsx
@@ -0,0 +1,15 @@
+import React, { Fragment } from "react";
+
+const NotFound = () => {
+  return (
+    <div>
+      <Fragment>
+        <h1 style={{ fontSize: 50 }}>
+          No Page Found for the Url you Searched.
+        </h1>
+      </Fragment>
+    </div>
+  );
+};
+
+export default NotFound;
